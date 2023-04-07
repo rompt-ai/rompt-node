c14a1b9d7455cee93c5c9c37d29a52acbf41b365
rompt-ai <hello@rompt.ai>
2023-04-06 20:41:33 -0500

    new bundle method

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,62 +1,30 @@
-import { getApiToken, debugLog } from '@romptai/common';
-import type { Prompts } from '@romptai/types';
-import { writeFileSync } from 'fs';
-import { join } from 'path';
-import fetch from 'cross-fetch';
+#! /usr/bin/env node
 
-interface Pull {
-    'branch'?: string;
-    'destination'?: string;
-    'apiToken'?: string;
-    '_env'?: string;
-    '_dry'?: boolean;
-}
+import { pull } from './pull';
+import arg from 'arg';
 
-export async function pull({
-    branch,
-    destination = 'prompts.json',
-    apiToken,
-    _env = 'prod',
-    _dry = false
-}: Pull): Promise<Prompts> {
-    const rootApi = _env ? `api-${_env}.aws.rompt.ai` : 'api.aws.rompt.ai';
-    const _apiToken = apiToken || getApiToken();
+const args = arg({
+    '--branch': String,
+    '--destination': String,
+    '--token': String,
+    '--_env': String,
+    '--_dry': Boolean,
 
-    debugLog(_env,
-        `Pulling prompts from branch ${branch}.`,
-        JSON.stringify({
-            branch,
-            destination,
-            env: _env,
-            apiToken: _apiToken,
-            rootApi,
-            cwd: process.cwd()
-        }, null, 2)
-    );
+    '-b': '--branch',
+    '-d': '--destination',
+});
 
-    const pullResult = await fetch(`https://${rootApi}/pull`, {
-        method: 'POST',
-        body: JSON.stringify({
-            apiToken: _apiToken,
-            branch,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((res) => res.json() as Promise<Prompts>);
-
-    if (!_dry) {
-        writeFileSync(join(process.cwd(), destination), JSON.stringify(pullResult, null, 2));
-        console.log(
-            `Done! Your prompts are in ${destination}.` +
-            `\n\nNext, install the \`@romptai/client\` package then use it in your code like this:` +
-            `\n\n\nconst romptData = generate("your-prompt-name", {\n  NAME: "Michael",\n  DIRECTION: "Generate a Tweet",\n  SENTIMENT: \`Make the Tweet about \$\{myOtherVariable\}\`\n})` +
-            `\n\nconst { prompt } = romptData;` +
-            `\n\n// Your generated prompt is in \`prompt\`` +
-            `\n\n// Example with OpenAI:` +
-            `\n\nconst gptResponse = await openai.createCompletion({\n  prompt,\n  //...\n});`,
-        );
-    }
-    
-    return pullResult;
-}
+pull({
+    branch: args['--branch'],
+    destination: args['--destination'],
+    apiToken: args['--token'],
+    _env: args['--_env'],
+    _dry: args['--_dry'],
+})
+    .then(() => {
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
---
+++ packages/cli/src/pull.ts
@@ -0,0 +1,62 @@
+import { getApiToken, debugLog } from '@romptai/common';
+import type { Prompts } from '@romptai/types';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+import fetch from 'cross-fetch';
+
+interface Pull {
+    'branch'?: string;
+    'destination'?: string;
+    'apiToken'?: string;
+    '_env'?: string;
+    '_dry'?: boolean;
+}
+
+export async function pull({
+    branch,
+    destination = 'prompts.json',
+    apiToken,
+    _env = 'prod',
+    _dry = false
+}: Pull): Promise<Prompts> {
+    const rootApi = _env ? `api-${_env}.aws.rompt.ai` : 'api.aws.rompt.ai';
+    const _apiToken = apiToken || getApiToken();
+
+    debugLog(_env,
+        `Pulling prompts from branch ${branch}.`,
+        JSON.stringify({
+            branch,
+            destination,
+            env: _env,
+            apiToken: _apiToken,
+            rootApi,
+            cwd: process.cwd()
+        }, null, 2)
+    );
+
+    const pullResult = await fetch(`https://${rootApi}/pull`, {
+        method: 'POST',
+        body: JSON.stringify({
+            apiToken: _apiToken,
+            branch,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then((res) => res.json() as Promise<Prompts>);
+
+    if (!_dry) {
+        writeFileSync(join(process.cwd(), destination), JSON.stringify(pullResult, null, 2));
+        console.log(
+            `Done! Your prompts are in ${destination}.` +
+            `\n\nNext, install the \`@romptai/client\` package then use it in your code like this:` +
+            `\n\n\nconst romptData = generate("your-prompt-name", {\n  NAME: "Michael",\n  DIRECTION: "Generate a Tweet",\n  SENTIMENT: \`Make the Tweet about \$\{myOtherVariable\}\`\n})` +
+            `\n\nconst { prompt } = romptData;` +
+            `\n\n// Your generated prompt is in \`prompt\`` +
+            `\n\n// Example with OpenAI:` +
+            `\n\nconst gptResponse = await openai.createCompletion({\n  prompt,\n  //...\n});`,
+        );
+    }
+    
+    return pullResult;
+}
